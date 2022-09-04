0c0d1365067daba28e5a7222542d98ade6ba877c
khushal ingle <khushalingle151@gmail.com>
2022-09-04 22:25:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var initialPrice = document.querySelector("#initial-price");
+var numberOfStocks = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var btnSubmit = document.querySelector("#submit-btn");
+var outputBox = document.querySelector("#output");
+
+btnSubmit.addEventListener('click', submitHandler);
+
+function submitHandler(){
+    var initial = initialPrice.value;
+    var quantity = numberOfStocks.value;
+    var current = currentPrice.value;
+    
+    if(initial==='' || quantity==='' || current===''){
+        showOutput(`Please fill out all the fields`);
+    }else{
+        initial = Number(initialPrice.value);
+        quantity = Number(numberOfStocks.value);
+        current = Number(currentPrice.value);
+        calculateProfitAndLoss(initial, quantity, current);   
+    }
+}
+function calculateProfitAndLoss(initial, quantity, current){
+    if(initial>current){
+        var loss = ((initial - current) * quantity).toFixed(2);
+        var lossPercent = ((loss/(initial*quantity)) * 100).toFixed(2);
+        showOutput(`Hey, the loss is ${loss} and the loss percentage is ${lossPercent}%`);
+        outputBox.style.color='red';
+       // outputBox.innerText = `Hey, the loss is ${loss} and the loss percentage is ${lossPercent}%`;
+    }else if(current>initial){
+        var profit = ((current - initial) * quantity).toFixed(2);
+        var profitPercent = ((profit/(initial*quantity)) * 100).toFixed(2);
+        showOutput(`Hey, the profit is ${profit} and the profit percentage is ${profitPercent}%`);
+        outputBox.style.color='green';
+        //outputBox.innerText = `Hey, the profit is ${profit} and the profit percentage is ${profitPercent}%`;
+    }else{
+        showOutput(`No gain no pain and no pain no gain`);
+        outputBox.style.color='blue';
+        //outputBox.innerText = `Hey, there is no gain no pain and no pain no gain`;
+    }
+}
+
+function showOutput(msg){
+    outputBox.innerText = msg;
+}
